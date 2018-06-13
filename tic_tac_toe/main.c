9072fbe8c882d85d98889efd73bc008fe674ffd8
John VanSchultz <johnvanschultz@upserve.com>
2018-06-13 17:51:44 -0400

    Initial Commit

---
+++ tic_tac_toe/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tic_tac_toe
+//
+//  Created by John VanSchultz on 6/13/18.
+//  Copyright © 2018 John VanSchultz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
